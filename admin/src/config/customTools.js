8b7d9138d8d4c18a3d0c838f86bcb1aba4cd75a9
pavel-kirpikau <pavel.kirpikau@gmail.com>
2022-10-06 13:06:20 +0300

    Update customTools.js
    Extend Embed functionality with Spotify, Mixcloud, Soundcloud.

--- admin/src/config/customTools.js
+++ admin/src/config/customTools.js
@@ -15,7 +15,50 @@ import Delimiter from '@editorjs/delimiter'
 import InlineCode from '@editorjs/inline-code'
 
 const customTools = {
-  embed: Embed,
+  embed: {
+    class: Embed,
+    config: {
+      services: {
+        youtube: true,
+        coub: true,
+        facebook: true,
+        twitter: true,
+        vimeo: true,
+        gfycat: true,
+        imgur: true,
+        spotifyshow: {
+          regex: /https?:\/\/open.spotify.com\/show\/(.+)/,
+          embedUrl: 'https://open.spotify.com/embed/show/<%= remote_id %>',
+          html: "<iframe style='border-radius:12px' border-width: '0px' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+          height: 352,
+          width: 644,
+          id: (groups) => groups.join('/embed/')
+          },
+        spotifyepisode: {
+          regex: /https?:\/\/open.spotify.com\/episode\/(.+)/,
+          embedUrl: 'https://open.spotify.com/embed/episode/<%= remote_id %>',
+          html: "<iframe style='border-radius:12px' border-width: '0px' width='100%' height='352' frameBorder='0' allowfullscreen='' allow='autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture' loading='lazy'></iframe>",
+          height: 352,
+          width: 644,
+          id: (groups) => groups.join('/embed/')  
+        },
+        mixcloudepisode: {
+          regex: /https?:\/\/www.mixcloud.com\/(.+)/,
+          embedUrl: 'https://www.mixcloud.com/widget/iframe/?hide_cover=1&feed=/<%= remote_id %>',
+          html: "<iframe width='100%'' height='120' frameborder='0' ></iframe>",
+          height: 120,
+          id: (groups) => groups.join('/embed/') 
+        },
+        soundcloudtrack: {
+          regex: /https?:\/\/soundcloud.com\/(.+).si.*/,
+          embedUrl: 'https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/<%= remote_id %>&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true',
+          html: "<iframe width='100%' height='300' scrolling='no' frameborder='no' allow='autoplay'></iframe>",
+          height: 300,
+          id: (groups) => groups.join('/embed/') 
+        }
+        }
+      }
+    },
   table: {
     class: Table,
     inlineToolbar: true,
